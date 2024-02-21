a1c06e7440720c97e6bf01d1699a7adb2cb5dd2e
Usamahafiz8 <usamahafiz8@gmail.com>
2024-02-21 12:48:24 +0500

    making backend as custom

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect('your-mongodb-uri', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
